0c258e83fa747ddea6232cda41a133a3c64580a6
Pokimonka <alenkaponomareva7@gmail.com>
2025-06-30 15:39:36 +0300

    goblin edit

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,31 @@
-(() => {
-  let active = 1;
-  const getPos = (index) => document.querySelector(`.cell${index}`);
-  const deleteGoblin = (index) =>
-    (getPos(index).className = `cell cell${index}`);
-  const activatePos = (index) => {
-    getPos(index).classList.add("has_goblin");
-  };
-  const next = () =>
-    setTimeout(() => {
-      deleteGoblin(active);
-      active = Math.floor(Math.random() * 16) + 1;
-      activatePos(active);
-      next();
-    }, 1000);
-  next();
-})();
+const size = 4;
+const field = document.querySelector(".field");
+
+if (field) {
+  for (let i = 0; i < size ** 2; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    field.appendChild(cell);
+  }
+}
+const cells = Array.from(document.querySelectorAll(".cell"));
+
+function SetgoblinImg(position, new_position) {
+  if (position !== -1) {
+    cells[position].classList.remove("has_goblin");
+  }
+  const goblinImgCell = cells[new_position];
+  goblinImgCell.classList.add("has_goblin");
+}
+
+setInterval(() => {
+  const position = cells.findIndex((item) =>
+    item.className.includes("has_goblin"),
+  );
+  let new_position = Math.floor(Math.random() * cells.length);
+  if (position === new_position) {
+    new_position = Math.floor(Math.random() * cells.length);
+  } else {
+    SetgoblinImg(position, new_position);
+  }
+}, 1000);
